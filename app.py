4ea6dd72833257ff5009fafe1155097bbb5b8c0b
DanielSet91 <Elessar.nazgul@gmail.com>
2024-01-12 17:14:14 +0200

    Debug, edited photos

--- app.py
+++ app.py
@@ -5,20 +5,20 @@ from modules.routes import configure_routes
 import secrets
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\\USER\\VsCode\\homework\\webuser\\instance\\users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\\USER\\VsCode\\homepage_talon\\instance\\users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 secret_key = secrets.token_hex(16)
 app.config["SECRET_KEY"] = secret_key
 app.config["SESSION_TYPE"] = "filesystem"
 
-login_manager.init_app(app)
 
 db = SQLAlchemy(app)
+login_manager.init_app(app)
 
 configure_routes(app)
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-
+    app.debug = True
     app.run(host="127.0.0.1", port=5000, debug=True)
